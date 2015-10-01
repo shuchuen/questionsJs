69d8b4c95115771849237f55acac863e1d438066
Shu Chuen Tam <shuchuen109@gmail.com>
2015-10-01 17:27:13 +0800

    add the firebase url and fix the typing mistake

--- js/controllers/todoCtrl.js
+++ js/controllers/todoCtrl.js
@@ -31,7 +31,7 @@ if (!roomId || roomId.length === 0) {
 }
 
 // TODO: Please change this URL for your app
-var firebaseURL = "https://classquestion.firebaseio.com/";
+var firebaseURL = "https://crackling-fire-8818.firebaseio.com/";
 
 
 $scope.roomId = roomId;
