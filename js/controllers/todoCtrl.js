af5f864fb1beffee57cee7911dbc34675734661d
Shu Chuen Tam <shuchuen109@gmail.com>
2015-10-05 00:53:41 +0800

    #milestone1: online user counter

--- js/controllers/todoCtrl.js
+++ js/controllers/todoCtrl.js
@@ -233,4 +233,59 @@ angular.element($window).bind("scroll", function() {
 	}
 });
 
-}]);
+}])
+
+.controller('onlineUserCtrl', ['$scope', 'onlineUserCountService','$location',
+    function($scope, onlineUserCountService) {
+      $scope.totalViewers = 0;
+
+      $scope.$on('onOnlineUser', function() {
+        $scope.$apply(function() {
+          $scope.totalViewers = onlineUserCountService.getOnlineUserCount();
+        });
+      });
+    }
+])
+
+.factory('onlineUserCountService', ['$rootScope', '$location',
+    function($rootScope, $location) {
+      var onlineUsers = 0;
+      var splits = $location.path().trim().split("/");
+      var roomId = angular.lowercase(splits[1]);
+      if (!roomId || roomId.length === 0) {
+        roomId = "all";
+      }
+        
+    var fireBaseUrl = 'https://crackling-fire-8818.firebaseio.com/';
+        
+      // Create our references
+      var listRef = new Firebase(fireBaseUrl+roomId+'/presence');
+      var userRef = listRef.push(); // This creates a unique reference for each user
+      var presenceRef = new Firebase(fireBaseUrl+'.info/connected');
+
+      // Add ourselves to presence list when online.
+      presenceRef.on('value', function(snap) {
+        if (snap.val()) {
+          userRef.set(true);
+          // Remove ourselves when we disconnect.
+          userRef.onDisconnect().remove();
+        }
+      });
+
+      // Get the user count and notify the application
+      listRef.on('value', function(snap) {
+        onlineUsers = snap.numChildren();
+        $rootScope.$broadcast('onOnlineUser');
+      });
+
+      var getOnlineUserCount = function() {
+        return onlineUsers;
+      }
+
+      return {
+        getOnlineUserCount: getOnlineUserCount
+      }
+    }
+]);
+
+//onlineUserCount ref: http://www.ng-newsletter.com/advent2013/#!/day/9
